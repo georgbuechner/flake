05a002043730a9479593378d97d99062d8888650
fux <jan.dick@bdp.org>
2023-03-11 02:00:48 +0100

    Fixes #146 -
    Escapes special latex characters for certain fields (mla, line, procedures, medication, ...).

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -139,3 +139,7 @@ def get_root_user(path):
     with open(path, "w") as f:
         json.dump(config, f)
     return root_email
+
+def escape_latex_string(txt: str) -> str: 
+    for c in ["#", "$", "%", "~", "_", "^", "\\"]:
+        txt = txt.replace(c, f"\{c}")
