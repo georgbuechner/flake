68c0c65d73b87aa2401cf4b76fc38db7b38f6717
fux <jan.dick@bdp.org>
2023-03-11 02:20:37 +0100

    Fixes missing returning escaped text

--- src/app.py
+++ src/app.py
@@ -744,6 +744,7 @@ def generate_paragraph_9(escaped_protocol: str):
     def safe(txt: str) -> str: 
         for c in ["#", "$", "%", "~", "_", "^"]:
             txt = txt.replace(c, f"\{c}")
+        return txt
     txt = render_template(
         "main.tex", subprotocols=subprotocols, protocol=protocol, safe=safe
     )
@@ -765,8 +766,8 @@ def generate_paragraph_9(escaped_protocol: str):
     proc=subprocess.Popen(
         ["pdflatex", full_path], 
         cwd=tmp_path, 
-        # stdout=subprocess.DEVNULL,
-        # stderr=subprocess.STDOUT
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT
     )
     proc.communicate()
     return send_file(f"{tmp_path}/main.pdf", as_attachment=True)
